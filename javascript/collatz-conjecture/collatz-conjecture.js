9234e32073c38485e1ccd5aa1049b9970f16787e
Jason <jasylwong@gmail.com>
2020-05-16 12:53:08 +0100

    add triangle exercise

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,12 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+export const steps = (n) => {
+  if (n <= 0) { throw new Error('Only positive numbers are allowed') }
+  let step_count = 0
+  while (n !== 1) {
+    n = (n % 2 === 0) ? (n/2) : (3 * n + 1)
+    step_count ++
+  }
+  return step_count
 };
