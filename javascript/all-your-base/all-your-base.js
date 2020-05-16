efa28d74770bfb04b6f36e81f907f4f32a113c9b
Jason <jasylwong@gmail.com>
2020-05-16 20:11:09 +0100

    complete all-your-base

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -1,8 +1,31 @@
-//
-// This is only a SKELETON file for the 'All Your Base' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const convert = (arr, a, b) => {
+  if (a <= 1 || a === undefined || a % 1 !== 0) { throw new Error('Wrong input base') }
+  if (b <= 1 || b === undefined || b % 1 !== 0) { throw new Error('Wrong output base') }
 
-export const convert = () => {
-  throw new Error("Remove this statement and implement this function");
+  if (arr.length === 0) { throw new Error('Input has wrong format') }
+  if (arr[0] === 0 && arr.length > 1) { throw new Error('Input has wrong format') }
+  if (arr.some(el => el < 0)) { throw new Error('Input has wrong format') }
+  if (arr.some(el => el >= a)) { throw new Error('Input has wrong format') }
+
+
+  const n = arr.length
+  const total = arr.map((x,i) => arr[i] * a**(n-i-1)).reduce((a,b) => a + b, 0)
+  if (total === 1 || total === 0) { return arr }
+  
+  let max_index = 0
+
+  while (total > b**max_index) {
+    max_index ++
+  }
+  
+  max_index --
+  
+  let result = []
+  let remainder = total
+  for (let i = 0; i <= max_index; i++) {
+    result.push(Math.floor(remainder / b**(max_index-i)))
+    remainder = remainder % b**(max_index-i)
+  }
+   
+  return result
 };
