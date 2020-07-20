4c9c2e5b3867a6c59b330c4d114c485e6cd02570
Jason <jasylwong@gmail.com>
2020-07-20 12:38:00 +0100

    refactor

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,6 +1,6 @@
 module Pangram
   ALL_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
   def self.pangram?(sentence)
-    sentence.gsub(/[^a-z]/i, '').downcase.split('').uniq.sort.join == ALL_LETTERS
+    ([*'a'..'z'] - sentence.downcase.chars).empty?
   end
 end
