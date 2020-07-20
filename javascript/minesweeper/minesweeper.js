99027710e198f1ebe0450bf39ae7d9c5e25b2312
Jason <jasylwong@gmail.com>
2020-07-20 17:28:06 +0100

    group rows

--- javascript/minesweeper/minesweeper.js
+++ javascript/minesweeper/minesweeper.js
@@ -5,14 +5,16 @@ export const annotate = (input) =>  {
       if (input[r][c] === "*") {
         output[r][c] = '*'
       } else {
-        if (r > 0 && c > 0 && input[r-1][c-1] === "*") {
-          output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
-        }
-        if (r > 0 && input[r-1][c] === "*") {
-          output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
-        }
-        if (r > 0 && c < input[0].length - 1 && input[r-1][c+1] === "*") {
-          output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
+        if (r > 0) {
+          if (c > 0 && input[r-1][c-1] === "*") {
+            output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
+          }
+          if (input[r-1][c] === "*") {
+            output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
+          }
+          if (c < input[0].length - 1 && input[r-1][c+1] === "*") {
+            output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
+          }
         }
         if (c > 0 && input[r][c-1] === "*") {
           output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
@@ -20,14 +22,16 @@ export const annotate = (input) =>  {
         if (c < input[0].length - 1 && input[r][c+1] === "*") {
           output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
         }
-        if (r < input.length - 1 && c > 0 && input[r+1][c-1] === "*") {
-          output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
-        }
-        if (r < input.length - 1 && input[r+1][c] === "*") {
-          output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
-        }
-        if (r < input.length - 1 && c < input[0].length - 1 && input[r+1][c+1] === "*") {
-          output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
+        if (r < input.length - 1) {
+          if (c > 0 && input[r+1][c-1] === "*") {
+            output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
+          }
+          if (input[r+1][c] === "*") {
+            output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
+          }
+          if (c < input[0].length - 1 && input[r+1][c+1] === "*") {
+            output[r][c] = (output[r][c] === ' ' ? 1 : output[r][c] + 1)
+          }
         }
       }
     }
