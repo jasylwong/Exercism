d170c9972d88c8cdccc3b5c9a1496d5b320a2104
Jason <jasylwong@gmail.com>
2020-07-27 22:47:55 +0100

    pass ruby high_scores personal best test

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,10 @@
+class HighScores
+  attr_reader :scores, :latest, :personal_best, :personal_top_three
+
+  def initialize(scores)
+    @scores = scores
+    @latest = scores[-1]
+    @personal_best = scores.max
+    @personal_top_three = scores.sort.last(3).reverse
+  end
+end
