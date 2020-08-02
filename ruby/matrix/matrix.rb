4f03e5c2859149a1c395ec0119d3cd574cc309f3
Jason <jasylwong@gmail.com>
2020-08-02 23:46:27 +0100

    pass ruby matrix column test

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -9,4 +9,14 @@ class Matrix
         r.split(" ").map{ |e| e.to_i }
       end
   end
+
+  def columns
+    output = Array.new(rows[0].length){[]}
+    for x in 0...rows[0].length
+      rows.each do |r|
+        output[x].push(r[x])
+      end
+    end
+    output
+  end
 end
