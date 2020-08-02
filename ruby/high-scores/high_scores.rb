7f8d6a24545cda44d45ea38f2cacbf081f65d2a7
Jason <jasylwong@gmail.com>
2020-08-02 20:06:11 +0100

    refactor ruby high_scores to use fewer attr_readers, and no store to memory

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,14 +1,23 @@
 class HighScores
-  attr_reader :scores, :latest, :personal_best, :personal_top_three
+  attr_reader :scores
 
   def initialize(scores)
     @scores = scores
-    @latest = scores.last
-    @personal_best = scores.max
-    @personal_top_three = scores.max(3)
+  end
+
+  def latest
+    scores.last
+  end
+
+  def personal_best
+    scores.max
+  end
+
+  def personal_top_three
+    scores.max(3)
   end
 
   def latest_is_personal_best?
-    @latest == @personal_best
+    latest == personal_best
   end
 end
