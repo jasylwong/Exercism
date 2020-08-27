b9d285f7524b75fafe84aeee8f061eda4cddee76
jasylwong <jasylwong@gmail.com>
2020-08-27 23:51:55 +0100

    complete Ruby triangle

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,8 +1,34 @@
-=begin
-Write your code for the 'Triangle' exercise in this file. Make the tests in
-`triangle_test.rb` pass.
+class Triangle
+  def initialize(sides)
+    @sides = sides
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/triangle` directory.
-=end
+  # Any suggestions on how I can DRY this by not 
+  # writing valid_sides? for each separate method?
 
+  def equilateral?
+    valid_sides? && @sides.uniq.length == 1
+  end
+
+  def isosceles?
+    valid_sides? && @sides.uniq.length <= 2
+  end
+
+  def scalene?
+    valid_sides? && @sides.uniq.length == 3
+  end
+
+  private
+
+  def valid_sides?
+     at_least_degenerate? && all_sides_greater_than_zero?
+  end
+
+  def at_least_degenerate?
+    @sides.max <= @sides.min(2).reduce(:+)
+  end
+
+  def all_sides_greater_than_zero?
+    @sides.all?{ |n| n > 0 }
+  end
+end
