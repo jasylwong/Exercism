75351d1bc42578e336e4711fb3348b2dbf97cf1d
jasylwong <jasylwong@gmail.com>
2020-08-27 22:59:34 +0100

    use .transpose to get columns from rows

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -11,25 +11,6 @@ class Matrix
   end
 
   def columns
-    output = Array.new(rows[0].length){[]}
-    for x in 0...rows[0].length
-      rows.each do |r|
-        output[x].push(r[x])
-      end
-    end
-    output
+    rows.transpose
   end
 end
-
-# Every time you see this pattern:
-
-# Initialize an object (like output)
-# Iterate and modify this object
-# Return this object
-# ... you probably have an Enumerable method to do the job.
-
-# And for, although a valid syntax, is not idiomatic in Ruby.
-
-# Can you try a way to rewrite columns using iterators? (Just if you want. Because I think this would be beyond the scope of this exercise).
-
-# And... You could just use Array#transpose at rows to get columns.
