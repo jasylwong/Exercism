966fa9dc22e59373c6ca089d195164ca4222bb3b
jasylwong <jasylwong@gmail.com>
2020-09-06 10:54:05 +0100

    freeze COLORS array constant

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,5 +1,5 @@
 module ResistorColor
-  COLORS = %w(black brown red orange yellow green blue violet grey white)
+  COLORS = %w(black brown red orange yellow green blue violet grey white).freeze
 
   def self.color_code(color)
     COLORS.index(color)
