7af1f45423f7a936c63ec29b30a34d9487d08ad2
jasylwong <jasylwong@gmail.com>
2020-11-11 21:22:47 +0000

    blank line

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,6 +1,7 @@
 module PhoneNumber
   def self.clean(full_number)
     number = full_number.delete('^0-9')
+    
     return if number.length == 11 && number[0] != '1'
     return if %w(0 1).include?(number[-10]) || %w(0 1).include?(number[-7])
 
