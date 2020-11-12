e6632d42207ad6e8691d7d0d3c7f4a2f41a627a7
jasylwong <jasylwong@gmail.com>
2020-11-12 20:57:24 +0000

    pass Ruby word_count test_handles_cramped_lists

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,11 +1,11 @@
 class Phrase
-  def initialize(words)
-    @words = words
+  def initialize(str)
+    @words = str.split(/[,\s]+/)
   end
 
   def word_count
     counts = {}
-    @words.split.each do |word|
+    @words.each do |word|
       counts[word] = counts.keys.include?(word) ? counts[word] + 1 : 1
       # if counts.keys.include?(word)
       #   counts[word] += 1
