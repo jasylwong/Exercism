d5a419d127a9ffd0347067491a2d9c7aac4e9fad
jasylwong <jasylwong@gmail.com>
2020-11-17 19:19:29 +0000

    pass Ruby hamming test_empty_strands

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'Hamming' exercise in this file. Make the tests in
-`hamming_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/hamming` directory.
-=end
-
+module Hamming
+  def self.compute(strand1, strand2)
+    strand1.split.each_with_index.map { |n, i| n != strand2.split[i] }.count(true)
+  end
+end
