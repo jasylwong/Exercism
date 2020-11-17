e6ff7203463acf90366a2bf90a2442133efdca20
jasylwong <jasylwong@gmail.com>
2020-11-17 20:48:46 +0000

    pass Ruby hamming test_disallow_first_strand_longer

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,7 @@
 module Hamming
   def self.compute(strand1, strand2)
+    raise ArgumentError if strand1.length != strand2.length
+
     strand1.chars.zip(strand2.chars).select{|a, b| a !=b }.count
   end
 end
