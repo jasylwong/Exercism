d721bd3c710dadec6b2bddebe722176909dea950
jasylwong <jasylwong@gmail.com>
2020-11-18 20:01:34 +0000

    pass Ruby nucleotide-count test_empty_dna_strand_has_no_adenosine

--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -1,8 +1,8 @@
-=begin
-Write your code for the 'Nucleotide Count' exercise in this file. Make the tests in
-`nucleotide_count_test.rb` pass.
+module Nucleotide
+  def self.from_dna(strand)
+    strand
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/nucleotide-count` directory.
-=end
-
+  def count
+  end
+end
