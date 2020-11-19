9095d4a348fc2264596fe56b51c55ad50be4bcef
jasylwong <jasylwong@gmail.com>
2020-11-19 20:43:00 +0000

    pass Ruby pythagorean-triplet test_sum

--- ruby/pythagorean-triplet/pythagorean_triplet.rb
+++ ruby/pythagorean-triplet/pythagorean_triplet.rb
@@ -1,8 +1,11 @@
-=begin
-Write your code for the 'Pythagorean Triplet' exercise in this file. Make the tests in
-`pythagorean_triplet_test.rb` pass.
+class Triplet
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/pythagorean-triplet` directory.
-=end
-
+  def sum
+    @a + @b + @c
+  end
+end
