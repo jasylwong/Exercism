5607ca56f5db834dd6aa99e34ad7c9346d788fcf
jasylwong <jasylwong@gmail.com>
2020-11-29 10:29:27 +0000

    pass Ruby raindrops test_the_sound_for_1_is_1

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
-
+module Raindrops
+  def self.convert(n)
+    "1"
+  end
+end
