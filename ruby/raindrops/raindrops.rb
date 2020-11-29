8a7893af7c59446adff935745fdd01d19fd7c625
jasylwong <jasylwong@gmail.com>
2020-11-29 10:34:47 +0000

    pass Ruby raindrops test_the_sound_for_7_is_plong

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -3,6 +3,7 @@ module Raindrops
     sound = ''
     sound += 'Pling' if n % 3 == 0
     sound += 'Plang' if n % 5 == 0
+    sound += 'Plong' if n % 7 == 0
     sound.empty? ? n.to_s : sound
   end
 end
