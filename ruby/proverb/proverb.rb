50f03f5f14b5e25f3bbd7cbc190e7854a21ae130
jasylwong <jasylwong@gmail.com>
2020-12-20 18:06:08 +0000

    pass Ruby proverb test_a_short_chain_of_consequences

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -1,9 +1,11 @@
 class Proverb
   def initialize(*words)
+    @words = words
   end
 
   def to_s
-    "For want of a nail the shoe was lost.\n" \
-    'And all for the want of a nail.'
+    (0..@words.length-2).map do |i|
+      "For want of a #{@words[i]} the #{@words[i+1]} was lost."
+    end.join("\n") + "\nAnd all for the want of a #{@words[0]}."
   end
 end
