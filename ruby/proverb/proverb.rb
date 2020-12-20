f78a32bb61c43ca54f8af25a5bd1ea53961ae966
jasylwong <jasylwong@gmail.com>
2020-12-20 18:27:05 +0000

    pass Ruby proverb test_an_optional_qualifier_in_the_final_consequence

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -1,11 +1,12 @@
 class Proverb
-  def initialize(*words)
+  def initialize(*words, qualifier: nil)
     @words = words
+    @qualifier = qualifier.nil? ? '' : qualifier + ' '
   end
 
   def to_s
     (0..@words.length-2).map do |i|
       "For want of a #{@words[i]} the #{@words[i+1]} was lost."
-    end.join("\n") + "\nAnd all for the want of a #{@words[0]}."
+    end.join("\n") + "\nAnd all for the want of a #{@qualifier}#{@words[0]}."
   end
 end
