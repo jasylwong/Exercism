776cc6b2392256fe9c1b675e1a3af94c84b3ccff
jasylwong <jasylwong@gmail.com>
2020-12-26 11:01:55 +0000

    change += to << to reduce creation of interim objects

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,9 +1,9 @@
 module Raindrops
   def self.convert(n)
     sound = ''
-    sound += 'Pling' if n % 3 == 0
-    sound += 'Plang' if n % 5 == 0
-    sound += 'Plong' if n % 7 == 0
+    sound << 'Pling' if n % 3 == 0
+    sound << 'Plang' if n % 5 == 0
+    sound << 'Plong' if n % 7 == 0
     sound.empty? ? n.to_s : sound
   end
 end
