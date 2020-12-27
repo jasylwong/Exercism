63dfe927b4317d511a2fa2b2085359e6dd18fd53
jasylwong <jasylwong@gmail.com>
2020-12-27 16:47:51 +0000

    move initialize to amongst private methods

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,13 +1,13 @@
 class Anagram
-  def initialize(phrase)
-    @phrase = phrase
-  end
-
   def match(anagrams)
     anagrams.select{ |a| letters_equal?(a) && !anagram_of_self?(a) }
   end
 
   private
+
+  def initialize(phrase)
+    @phrase = phrase
+  end
 
   def letters_equal?(phrase)
     phrase.downcase.chars.sort == @phrase.downcase.chars.sort
