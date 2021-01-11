32dba85f20dcb703cc2e68b14e1dca8291d313c2
jasylwong <jasylwong@gmail.com>
2021-01-11 00:12:45 +0000

    pass Ruby matching_brackets test_paired_square_brackets

--- ruby/matching-brackets/matching_brackets.rb
+++ ruby/matching-brackets/matching_brackets.rb
@@ -1,8 +1,11 @@
-=begin
-Write your code for the 'Matching Brackets' exercise in this file. Make the tests in
-`matching_brackets_test.rb` pass.
+module Brackets
+  def self.paired?(string)
+    string = string.split('')
 
-To get started with TDD, see the `README.md` file in your
-`ruby/matching-brackets` directory.
-=end
-
+    return false if string.count('[') != string.count(']')
+    
+    (0...string.length).each do |i|
+      return false if string[i..-1].count('[') > string[i..-1].count(']')
+    end
+  end
+end
--- ruby/matching-brackets/matching_brackets_test.rb
+++ ruby/matching-brackets/matching_brackets_test.rb
@@ -4,7 +4,6 @@ require_relative 'matching_brackets'
 # Common test data version: 1.5.0 20dd164
 class MatchingBracketsTest < Minitest::Test
   def test_paired_square_brackets
-    # skip
     assert Brackets.paired?('[]')
   end
 
