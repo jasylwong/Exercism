a890f9744106928a7a9fd64288f673cdb04ec20d
jasylwong <jasylwong@gmail.com>
2021-01-16 18:03:32 +0000

    pass Ruby perfect-numbers other tests

--- ruby/perfect-numbers/perfect_numbers.rb
+++ ruby/perfect-numbers/perfect_numbers.rb
@@ -1,5 +1,15 @@
 module PerfectNumber
   def self.classify(int)
     raise RuntimeError if int < 0
+
+    factors_sum = (1..int-1).select{ |i| int % i == 0 }.reduce(:+)
+
+    if factors_sum < int
+      'deficient'
+    elsif factors_sum == int
+      'perfect'
+    else
+      'abundant'
+    end
   end
 end
