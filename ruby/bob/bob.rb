f00dca5afc60cc6f88d17ff269865e435e3b63dc
jasylwong <jasylwong@gmail.com>
2021-01-23 15:37:20 +0000

    pass Ruby bob test_ending_with_whitespace

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -4,7 +4,7 @@ module Bob
       "Calm down, I know what I'm doing!"
     elsif remark.upcase == remark && remark.count("a-zA-Z") > 0
       'Whoa, chill out!'
-    elsif remark[-1] == '?'
+    elsif remark.strip[-1] == '?'
       'Sure.'
     elsif remark.strip.empty?
       'Fine. Be that way!'
