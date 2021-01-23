2c9a640dbd28358df254f502df0beceb6f046448
jasylwong <jasylwong@gmail.com>
2021-01-23 15:41:48 +0000

    refactor Ruby bob;

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,12 +1,12 @@
 module Bob
   def self.hey(remark)
-    if remark.upcase == remark && remark[-1] == '?' && remark.count("a-zA-Z") > 0
-      "Calm down, I know what I'm doing!"
-    elsif remark.upcase == remark && remark.count("a-zA-Z") > 0
-      'Whoa, chill out!'
-    elsif remark.strip[-1] == '?'
+    remark = remark.strip
+
+    if remark.upcase == remark && remark.count("a-zA-Z") > 0
+      remark[-1] == '?' ? "Calm down, I know what I'm doing!" : 'Whoa, chill out!'
+    elsif remark[-1] == '?'
       'Sure.'
-    elsif remark.strip.empty?
+    elsif remark.empty?
       'Fine. Be that way!'
     else
       'Whatever.'
