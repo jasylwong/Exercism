5818bb24cb525e7040017dd4a626cf41f4c94172
jasylwong <jasylwong@gmail.com>
2021-01-25 22:39:48 +0000

    refactor Ruby accumulate using .map

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,11 +1,6 @@
 class Array
   def accumulate(&block)
-    # # i = 0
-    # # while i < size
-    # #   block.call at(i)
-    # #   i += 1
-    # # end
-    # block.call(yield)
-    self.each_with_index { |n, i| self[i] = yield(n) }
+    # self.each_with_index { |n, i| self[i] = yield(n) }
+    self.map { |n| yield(n) }
   end
 end
