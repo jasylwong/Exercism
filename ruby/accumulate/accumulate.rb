3877549176ec5432e5eb70fb0974629d770c91bd
jasylwong <jasylwong@gmail.com>
2021-01-25 21:44:54 +0000

    pass Ruby accumulate test_empty_accumulation

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'Accumulate' exercise in this file. Make the tests in
-`accumulate_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/accumulate` directory.
-=end
-
+class Array
+  def accumulate
+    []
+  end
+end
