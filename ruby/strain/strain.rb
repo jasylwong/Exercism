7a21ab7dd061b1299867fb7e86f2eaea4d9cc158
jasylwong <jasylwong@gmail.com>
2021-01-26 21:21:46 +0000

    pass Ruby strain test_empty_keep

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'Strain' exercise in this file. Make the tests in
-`strain_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/strain` directory.
-=end
-
+class Array
+  def keep(&block)
+    map { |i| yield(i) }
+  end
+end
