54fecd58beb38dbcc54ae275db5bcb718b15d7a3
jasylwong <jasylwong@gmail.com>
2021-01-30 17:04:04 +0000

    use constant BOARD_SQUARES for magical numbers

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,11 +1,13 @@
 module Grains
+  BOARD_SQUARES = 1..64
+
   def self.square(location)
-    raise ArgumentError if !location.between?(1, 64)
+    raise ArgumentError if !BOARD_SQUARES.include? location
 
     2 ** (location - 1)
   end
 
   def self.total
-    [*1..64].map { |s| self.square(s) }.sum
+    [*BOARD_SQUARES].map { |s| self.square(s) }.sum
   end
 end
