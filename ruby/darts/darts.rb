7be027423711706117449e8c045878b37ed52151
jasylwong <jasylwong@gmail.com>
2021-01-30 16:44:26 +0000

    use attr_reader for distance_from_centre

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -1,14 +1,16 @@
 class Darts
+  attr_reader :distance_from_centre
+
   def initialize(x, y)
     @distance_from_centre = Math.sqrt(x**2 + y**2)
   end
 
   def score
-    if @distance_from_centre <= 1
+    if distance_from_centre <= 1
       10
-    elsif @distance_from_centre <= 5
+    elsif distance_from_centre <= 5
       5
-    elsif @distance_from_centre <= 10
+    elsif distance_from_centre <= 10
       1
     else
       0
