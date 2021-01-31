2c0ebe8e1f7dbd50920e8b31958a64bcb942f98a
jasylwong <jasylwong@gmail.com>
2021-01-31 16:04:09 +0000

    use hash instead of conditional

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -1,20 +1,14 @@
 class Darts
   attr_reader :distance_from_centre
 
+  SCORING_SYSTEM = { (0..1) => 10, (1..5) => 5, (5..10) => 1, (10..) => 0 }
+
   def initialize(x, y)
     @distance_from_centre = Math.sqrt(x**2 + y**2)
   end
 
   def score
-    case distance_from_centre
-    when 0..1
-      10
-    when 1..5
-      5
-    when 5..10
-      1
-    else
-      0
-    end
+    SCORING_SYSTEM.select{ |k, _v| k.include? distance_from_centre }
+                  .values.first
   end
 end
