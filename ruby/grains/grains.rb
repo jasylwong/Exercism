8af2f03572f078f4a5d3b0654734cdc510c6acf8
jasylwong <jasylwong@gmail.com>
2021-01-31 17:35:38 +0000

    change BOARD_SQUARES to integer rather than range

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,14 +1,14 @@
 module Grains
-  BOARD_SQUARES = 1..64
+  BOARD_SQUARES = 64
   GRAIN_MULTIPLIER = 2
 
   def self.square(board_location)
-    raise ArgumentError unless BOARD_SQUARES.include? board_location
+    raise ArgumentError unless (1..BOARD_SQUARES).include? board_location
 
     GRAIN_MULTIPLIER ** (board_location - 1)
   end
 
   def self.total
-    GRAIN_MULTIPLIER ** BOARD_SQUARES.last - 1
+    GRAIN_MULTIPLIER ** BOARD_SQUARES - 1
   end
 end
