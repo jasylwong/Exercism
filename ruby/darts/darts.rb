acbeb7e951d5cb3f0df4f113d31e08dda8c2ea26
jasylwong <jasylwong@gmail.com>
2021-02-01 21:30:10 +0000

    refactor TARGET hash

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -1,14 +1,19 @@
 class Darts
   attr_reader :distance_from_centre
 
-  SCORING_SYSTEM = { (0..1) => 10, (1..5) => 5, (5..10) => 1, (10..) => 0 }
+  TARGET = {
+    0..1 => 10,
+    1..5 => 5,
+    5..10 => 1,
+    10.. => 0
+  }
 
   def initialize(x, y)
     @distance_from_centre = Math.sqrt(x**2 + y**2)
   end
 
   def score
-    SCORING_SYSTEM.select{ |k, _v| k.include? distance_from_centre }
+    TARGET.select{ |k, _v| k.include? distance_from_centre }
                   .values.first
   end
 end
