974512014b43f46c7ca57db9ccb23170f9588e68
jasylwong <jasylwong@gmail.com>
2021-02-06 17:08:33 +0000

    remove spaces either side of ** indices

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -5,10 +5,10 @@ module Grains
   def self.square(board_location)
     raise ArgumentError unless (1..BOARD).include? board_location
 
-    GRAIN_MULTIPLIER ** (board_location - 1)
+    GRAIN_MULTIPLIER**(board_location - 1)
   end
 
   def self.total
-    GRAIN_MULTIPLIER ** BOARD - 1
+    GRAIN_MULTIPLIER**BOARD - 1
   end
 end
