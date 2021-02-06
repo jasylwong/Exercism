e282dc28cf1e6cb7d2d91353431799ea1ef85828
jasylwong <jasylwong@gmail.com>
2021-02-06 17:07:44 +0000

    change BOARD_SQUARES constant to BOARD

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,14 +1,14 @@
 module Grains
-  BOARD_SQUARES = 64
+  BOARD = 64
   GRAIN_MULTIPLIER = 2
 
   def self.square(board_location)
-    raise ArgumentError unless (1..BOARD_SQUARES).include? board_location
+    raise ArgumentError unless (1..BOARD).include? board_location
 
     GRAIN_MULTIPLIER ** (board_location - 1)
   end
 
   def self.total
-    GRAIN_MULTIPLIER ** BOARD_SQUARES - 1
+    GRAIN_MULTIPLIER ** BOARD - 1
   end
 end
