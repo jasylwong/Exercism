5b6c084e5f699255c35c2dc47a8875a613157890
jasylwong <jasylwong@gmail.com>
2021-04-03 21:09:03 +0100

    pass Ruby allergies test_is_allergic_to_eggs

--- ruby/allergies/allergies.rb
+++ ruby/allergies/allergies.rb
@@ -1,7 +1,16 @@
 class Allergies
+  ITEMS = { 1 => 'eggs' }
+
   def initialize(score)
+    @score = score
+    @allergies = []
   end
 
   def allergic_to?(item)
+    ITEMS.keys.each do |i|
+      @allergies.push(ITEMS[i]) if @score >= i
+    end
+
+    @allergies.include?(item)
   end
 end
