c28a0e32af62f26f9ca741330ec0a950b86757d6
jasylwong <jasylwong@gmail.com>
2021-04-05 11:59:04 +0100

    add ruby scrabble private constant and freeze

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -3,7 +3,8 @@ class Scrabble
     "A" => 1, "B" => 3, "C" => 3, "D" => 2, "E" => 1, "F" => 4, "G" => 2, "H" => 4, "I" => 1,
     "J" => 1, "K" => 5, "L" => 1, "M" => 3, "N" => 1, "O" => 1, "P" => 3, "Q" => 10, "R" => 1,
     "S" => 1,"T" => 1,"U" => 1,"V" => 4,"W" => 4,"X" => 8,"Y" => 4,"Z" => 10, '' => 0
-  }
+  }.freeze
+  private_constant :LETTER_VALUES
 
   def initialize(word)
     @word = word
