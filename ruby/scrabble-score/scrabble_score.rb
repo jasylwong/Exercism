bb1f79855ceea8b0aca86af654885dd0c447e57f
jasylwong <jasylwong@gmail.com>
2021-04-05 22:54:31 +0100

    pass ruby test_convenient_scoring

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,8 +1,9 @@
 class Scrabble
   LETTER_VALUES = {
-    "A" => 1, "B" => 3, "C" => 3, "D" => 2, "E" => 1, "F" => 4, "G" => 2, "H" => 4, "I" => 1,
-    "J" => 1, "K" => 5, "L" => 1, "M" => 3, "N" => 1, "O" => 1, "P" => 3, "Q" => 10, "R" => 1,
-    "S" => 1,"T" => 1,"U" => 1,"V" => 4,"W" => 4,"X" => 8,"Y" => 4,"Z" => 10, '' => 0
+    "A" => 1, "B" => 3, "C" => 3, "D" => 2, "E" => 1, "F" => 4, "G" => 2,
+    "H" => 4, "I" => 1, "J" => 1, "K" => 5, "L" => 1, "M" => 3, "N" => 1,
+    "O" => 1, "P" => 3, "Q" => 10, "R" => 1, "S" => 1,"T" => 1,"U" => 1,
+    "V" => 4,"W" => 4,"X" => 8,"Y" => 4,"Z" => 10
   }.freeze
   private_constant :LETTER_VALUES
 
@@ -11,8 +12,12 @@ class Scrabble
   end
 
   def score
-    return 0 if @word.to_s[/^[a-zA-Z]+$/].nil?
+    self.class.score(@word)
+  end
 
-    @word.chars.map { |c| LETTER_VALUES[c.upcase] }.sum
+  def self.score(word)
+    return 0 if word.to_s[/^[a-zA-Z]+$/].nil?
+
+    word.chars.map { |c| LETTER_VALUES[c.upcase] }.sum
   end
 end
