f28358d691d3c975606ad210936395894727428e
jasylwong <jasylwong@gmail.com>
2021-04-05 11:54:55 +0100

    pass ruby scrabble test_scores_very_short_word

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,8 +1,17 @@
 class Scrabble
+  LETTER_VALUES = {
+    "A" => 1, "B" => 3, "C" => 3, "D" => 2, "E" => 1, "F" => 4, "G" => 2, "H" => 4, "I" => 1,
+    "J" => 1, "K" => 5, "L" => 1, "M" => 3, "N" => 1, "O" => 1, "P" => 3, "Q" => 10, "R" => 1,
+    "S" => 1,"T" => 1,"U" => 1,"V" => 4,"W" => 4,"X" => 8,"Y" => 4,"Z" => 10, '' => 0
+  }
+
   def initialize(word)
+    @word = word
   end
 
   def score
-    0
+    return 0 if @word.to_s[/^[a-zA-Z]+$/].nil?
+
+    @word.chars.map { |c| LETTER_VALUES[c.upcase] }.sum
   end
 end
