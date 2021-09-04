ef7e6015504728d725ea6873e0470a5122707738
jasylwong <jasylwong@gmail.com>
2021-09-04 16:45:56 +0100

    defer work to model instance

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,23 +1,32 @@
 class Scrabble
-  LETTER_VALUES = {
-    "A" => 1, "B" => 3, "C" => 3, "D" => 2, "E" => 1, "F" => 4, "G" => 2,
-    "H" => 4, "I" => 1, "J" => 1, "K" => 5, "L" => 1, "M" => 3, "N" => 1,
-    "O" => 1, "P" => 3, "Q" => 10, "R" => 1, "S" => 1,"T" => 1,"U" => 1,
-    "V" => 4,"W" => 4,"X" => 8,"Y" => 4,"Z" => 10
-  }.freeze
-  private_constant :LETTER_VALUES
+  TILES = {
+    A: 1, N: 1,
+    B: 3, O: 1,
+    C: 3, P: 3,
+    D: 2, Q: 10,
+    E: 1, R: 1,
+    F: 4, S: 1,
+    G: 2, T: 1,
+    H: 4, U: 1,
+    I: 1, V: 4,
+    J: 1, W: 4,
+    K: 5, X: 8,
+    L: 1, Y: 4,
+    M: 3, Z: 10,
+    "": 0
+  }
 
   def initialize(word)
     @word = word
   end
 
   def score
-    self.class.score(@word)
+    return 0 if @word.to_s[/^[a-zA-Z]+$/].nil?
+
+    @word.chars.map { |c| TILES[c.upcase.to_sym] }.sum
   end
 
   def self.score(word)
-    return 0 if word.to_s[/^[a-zA-Z]+$/].nil?
-
-    word.chars.map { |c| LETTER_VALUES[c.upcase] }.sum
+    new(word).score
   end
 end
