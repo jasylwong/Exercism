9f5c4566c659fa4f00c6d712c104f46f1a2ebd5d
jasylwong <jasylwong@gmail.com>
2021-09-04 21:53:07 +0100

    refactor Ruby assembly-line

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,27 +1,30 @@
 class AssemblyLine
-  PRODUCTION_RATE_SPEED_ONE = 221
-  SUCCESS_RATES = {
-    1 => 1,
-    2 => 1,
-    3 => 1,
-    4 => 1,
-    5 => 0.9,
-    6 => 0.9,
-    7 => 0.9,
-    8 => 0.9,
-    9 => 0.8,
-    10 => 0.77
-  }
+  PEAK_PRODUCTION_RATE_SPEED_ONE = 221
 
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    PRODUCTION_RATE_SPEED_ONE * @speed * SUCCESS_RATES[@speed]
+    PEAK_PRODUCTION_RATE_SPEED_ONE * @speed * success_rate
   end
 
   def working_items_per_minute
     (production_rate_per_hour / 60).floor
+  end
+
+  private
+
+  def success_rate
+    case @speed
+    when 1..4
+      1
+    when 5..8
+      0.9
+    when 9
+      0.8
+    when 10
+      0.77
+    end
   end
 end
