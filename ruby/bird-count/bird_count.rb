a67879c4987593a8c9e8972888d16e9f8bb147ce
jasylwong <jasylwong@gmail.com>
2021-09-05 14:10:16 +0100

    refactor ruby bird_count

--- ruby/bird-count/bird_count.rb
+++ ruby/bird-count/bird_count.rb
@@ -1,27 +1,27 @@
 class BirdCount
-  attr_reader :last_week
+  attr_reader :birds_per_day
 
   def self.last_week
-    [0, 2, 5, 3, 7, 8, 4]
+    @birds_per_day || [0, 2, 5, 3, 7, 8, 4]
   end
 
   def initialize(birds_per_day)
-    @last_week = birds_per_day
+    @birds_per_day = birds_per_day
   end
 
   def yesterday
-    last_week[-2]
+    birds_per_day[-2]
   end
 
   def total
-    last_week.reduce(:+)
+    birds_per_day.sum
   end
 
   def busy_days
-    last_week.count { |count| count >= 5 }
+    birds_per_day.count { |birds| birds >= 5 }
   end
 
   def day_without_birds?
-    last_week.any? { |count| count == 0 }
+    birds_per_day.include? 0
   end
 end
